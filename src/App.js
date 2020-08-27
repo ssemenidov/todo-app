7e795a451a11deaef645466f37f52d090b4eb4c2
ssemenidov <23sergey03@gmail.com>
2020-08-27 20:18:38 +0800

    hosting + todo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Button, TextField } from "@material-ui/core";
+import "./App.css";
 import Todo from "./componets/Todo";
 import db from "./firebase";
+import firebase from "firebase";
 function App() {
   const [todos, setTodos] = useState([{}]);
   const [input, setInput] = useState("");
   useEffect(() => {
-    db.collection("todos").onSnapshot((snaphot) => {
-      console.log(snaphot.docs.map((doc) => doc.data()));
-      setTodos(snaphot.docs.map((doc) => doc.data()));
-    });
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snaphot) => {
+        setTodos(
+          snaphot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+        );
+      });
   }, []);
   const addTodo = (e) => {
     e.preventDefault();
     db.collection("todos").add({
       title: input,
       checked: false,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
     setInput("");
   };
@@ -43,7 +49,7 @@ function App() {
 
       <ul>
         {todos.map((todo) => (
-          <Todo todo={todo.title} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </ul>
     </div>
--- src/componets/Todo.js
+++ src/componets/Todo.js
@@ -1,4 +1,7 @@
 import React from "react";
+import db from "../firebase";
+import { IconButton } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import {
   List,
   ListItem,
@@ -7,13 +10,19 @@ import {
   ListItemText,
 } from "@material-ui/core";
 function Todo(props) {
+  const deleteTodo = (event) => {
+    db.collection("todos").doc(props.todo.id).delete();
+  };
   return (
     <List>
       <ListItem>
         <ListItemIcon>
           <Checkbox />
         </ListItemIcon>
-        <ListItemText primary={props.todo} />
+        <ListItemText primary={props.todo.title} />
+        <IconButton color="primary" component="span" onClick={deleteTodo}>
+          <DeleteForeverIcon />
+        </IconButton>
       </ListItem>
     </List>
   );
